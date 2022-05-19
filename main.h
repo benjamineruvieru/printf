10f4441f2e302d8a7fb5cee1ada484d908ed35bd
Nechezzz <ilobigift@gmail.com>
2022-05-19 12:47:26 -0700

    update main.h

--- main.h
+++ main.h
@@ -1,8 +1,33 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 #include <stdlib.h>
 #include <stdarg.h>
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
 
 int _printf(const char *format, ...);
 int _puts(char *str);
