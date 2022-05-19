3a7e6bb10406899ecc96598204f90f5eb56b9bed
Benjamin3443 <101414331+Benjamin3443@users.noreply.github.com>
2022-05-19 20:08:19 +0100

    Update main.h

--- main.h
+++ main.h
@@ -4,6 +4,34 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+
+/* print_nums */
+
 int _printf(const char *format, ...);
 int _puts(char *str);
 int _putchar(char c);
@@ -11,7 +39,7 @@ char *convert(unsigned long int num, int base, int lowercase);
 int print_bigS(va_list l, flags_t *f);
 int get_flag(char s, flags_t *f);
 int print_rev(va_list l, flags_t *f);
-int(*get_print(char s))(va_list l, flags_t *f);
+int(*get_print(char s))(va_list, flags_t *);
 int print_int(va_list l, flags_t *f);
 void print_number(int n);
 int print_unsigned(va_list l, flags_t *f);
@@ -24,5 +52,6 @@ int print_char(va_list l, flags_t *f);
 int print_rot13(va_list l, flags_t *f);
 int print_address(va_list l, flags_t *f);
 int print_percent(va_list l, flags_t *f);
+int count_digit(int i);
 
 #endif
